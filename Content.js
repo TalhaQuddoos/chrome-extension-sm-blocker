59aa938de6e90549c5b61871ab63fda755226279
Talha Quddoos <talhaquddoos1@gmail.com>
2022-01-08 16:50:03 +0500

    just some updates

--- Content.js
+++ Content.js
@@ -13,20 +13,15 @@ function checkUrlChanged() {
 }
 
 function onVideoChange() {
-  const ms = Date.now();
   fetch(
-    `https://raw.githubusercontent.com/TalhaQuddoos/chrome-extension-sm-blocker/master/data.json?xyz=${ms}`
+    `https://mocki.io/v1/a158ad2f-c316-4f80-b296-88952ac3caef`
   )
     .then((response) => response.json())
     .then((data) => {
       const title = document.title;
       const channel = "---";
-      console.log(url);
       const restricted_urls = data["urls"];
-      console.log(restricted_urls);
-      window.tempor = restricted_urls;
-
-      console.log(window.tempor.includes(url));
+      console.log(restricted_urls)
       if (restricted_urls.includes(url)) {
         console.log("Caution!: This URL is restricted.");
       }
