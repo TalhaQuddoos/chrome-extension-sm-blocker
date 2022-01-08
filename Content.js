aaf36726ef483fc85d1a1f191a5707f543a5c3dc
Talha Quddoos <talhaquddoos1@gmail.com>
2022-01-08 20:55:51 +0500

    update

--- Content.js
+++ Content.js
@@ -1,32 +1,34 @@
 var url = window.location.href;
-onVideoChange()
-var interval = setInterval(checkUrlChanged, 1000)
+onVideoChange();
+var interval = setInterval(checkUrlChanged, 1000);
 
 function checkUrlChanged() {
-    clearInterval(interval);
-    interval = setInterval(checkUrlChanged, 1000);
+  clearInterval(interval);
+  interval = setInterval(checkUrlChanged, 1000);
 
-    var newUrl = window.location.href;
-    if (newUrl == url) return false;
-    url = newUrl;
-    onVideoChange();
+  var newUrl = window.location.href;
+  if (newUrl == url) return false;
+  url = newUrl;
+  onVideoChange();
 }
 
 function onVideoChange() {
-    fetch("https://raw.githubusercontent.com/TalhaQuddoos/chrome-extension-sm-blocker/master/data.json")
-    .then(response => response.json())
-    .then(data => {
-        const title = document.title
-        const channel = "---"
-        console.log(url)
-        const restricted_urls = data["urls"]
-        console.log(restricted_urls)
-        window.tempor = restricted_urls
-        
-        console.log(window.tempor.includes(url))
-        if(restricted_urls.includes(url)) {
-            console.log("Caution!: This URL is restricted.")
-        }
-    })
+  const ms = Date.now();
+  fetch(
+    `https://raw.githubusercontent.com/TalhaQuddoos/chrome-extension-sm-blocker/master/data.json?xyz=${ms}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const title = document.title;
+      const channel = "---";
+      console.log(url);
+      const restricted_urls = data["urls"];
+      console.log(restricted_urls);
+      window.tempor = restricted_urls;
 
+      console.log(window.tempor.includes(url));
+      if (restricted_urls.includes(url)) {
+        console.log("Caution!: This URL is restricted.");
+      }
+    });
 }
