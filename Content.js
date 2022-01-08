3f29a5b786d4ccdd64ce861932602f3d312fe251
Talha Quddoos <talhaquddoos1@gmail.com>
2022-01-08 17:03:15 +0500

    just some updates

--- Content.js
+++ Content.js
@@ -13,8 +13,16 @@ function checkUrlChanged() {
 }
 
 function onVideoChange() {
+  var myHeaders = new Headers();
+  myHeaders.append('pragma', 'no-cache');
+  myHeaders.append('cache-control', 'no-cache');
+  myHeaders.append("Access-Control-Allow-Origin", "*")
+  myHeaders.append("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+  myHeaders.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+
+  const ms = Date.now();
   fetch(
-    `https://mocki.io/v1/a158ad2f-c316-4f80-b296-88952ac3caef`
+    `https://raw.githubusercontent.com/TalhaQuddoos/chrome-extension-sm-blocker/master/data.json?b${ms}a=${ms}`
   )
     .then((response) => response.json())
     .then((data) => {
